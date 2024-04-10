620bc657db240af93a497b73ace18a6c2b575c28
RohanA <rohancp2@gmail.com>
2024-04-10 11:57:22 +1000

    add github actions and update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  basePath: "/react-portfolio",
   output: 'export',
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
